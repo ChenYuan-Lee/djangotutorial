11824fd90ea6cc94e49f0fb0ccc4e64cc43633b4
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-03-09 18:26:51 +0800

    Update index and detail views and templates

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse
+from typing import Union
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
 def index(request) -> HttpResponse:
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join(q.question_text for q in latest_question_list)
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id: int) -> HttpResponse:
-    return HttpResponse(f"You're looking at question {question_id}")
+def detail(request, question_id: int) -> Union[HttpResponse, Http404]:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id: int) -> HttpResponse:
     response = "You're looking at the results of question %s."
