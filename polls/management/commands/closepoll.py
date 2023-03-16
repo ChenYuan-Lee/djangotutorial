84814d5fc4f6cdbe0cecf41ce3f58246ee604bfb
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-03-16 16:17:38 +0800

    Add option to delete poll via closepoll command

--- polls/management/commands/closepoll.py
+++ polls/management/commands/closepoll.py
@@ -7,7 +7,15 @@ class Command(BaseCommand):
     help = "Closes the specified poll for voting"
 
     def add_arguments(self, parser: ArgumentParser):
+        # Positional args
         parser.add_argument("poll_ids", nargs="+", type=int)
+
+        # Named (optional) args
+        parser.add_argument(
+            "--delete",
+            action="store_true",
+            help="delete polls instead of closing it",
+        )
 
     def handle(self, *args, **options):
         for poll_id in options["poll_ids"]:
@@ -16,12 +24,19 @@ class Command(BaseCommand):
             except Poll.DoesNotExist:
                 raise CommandError('Poll "%s" does not exist' % poll_id)
 
-            if not poll.is_open:
-                raise CommandError('Poll "%s" is already closed' % poll_id)
+            if options["delete"]:
+                poll.delete()
+                
+                self.stdout.write(
+                    self.style.SUCCESS('Successfully deleted poll "%s"' % poll_id)
+                )
+            else:
+                if not poll.is_open:
+                    raise CommandError('Poll "%s" is already closed' % poll_id)
             
-            poll.is_open = False
-            poll.save()
+                poll.is_open = False
+                poll.save()
 
-            self.stdout.write(
-                self.style.SUCCESS('Successfully closed poll "%s"' % poll_id)
-            )
+                self.stdout.write(
+                    self.style.SUCCESS('Successfully closed poll "%s"' % poll_id)
+                )
